ed35c468ab9eb53d7cade24d68679083a6e25049
oguzh <oguzh@DESKTOP-LSF3P7J>
2024-02-18 07:52:53 +0300

    Add project files.

---
+++ Core/YoutubeApi.Application/Features/Auth/Command/Login/LoginCommandRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System.ComponentModel;
+
+namespace YoutubeApi.Application.Features.Auth.Command.Login
+{
+    public class LoginCommandRequest : IRequest<LoginCommandResponse>
+    {
+        [DefaultValue("oguzhanalmus@outlook.com")]
+        public string Email { get; set; }
+        [DefaultValue("123456")]
+        public string Password { get; set; }
+    }
+}
